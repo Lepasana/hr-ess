cda1a075c1d3f8618e91fa714284080bb052793b
Rogem <rogemlepasana@gmail.com>
2024-10-20 19:20:47 -0700

    Initial commit

---
+++ resources/views/vendor/filament/components/brand.blade.php
@@ -0,0 +1,6 @@
+<div class="flex items-center space-x-4">
+    <img src="{{ asset('/images/brand_logo.webp') }}" alt="Logo" class="h-10">
+    <span class="font-bold text-[#4F46E5] ">
+        ESS
+    </span>
+</div>
